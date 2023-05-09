36fdf1d2e32e490b3321d0229315b09e775740c3
Ярослав Эрлих <i.erlikh@rt-solar.ru>
2023-05-09 14:19:06 +0500

    add jenkins

--- Jenkins.groovy
+++ Jenkins.groovy
@@ -8,12 +8,88 @@ def CalculateFilesCount() {
 
 def checkout(name) {
     echo "Performing Git checkout"
+    git branch: name, credentialsId: 'github', url: 'git@github.com:YaroslavErlikh/groovyProjectWork.git'
+}
 
-    git branch: name, credentialsId: 'github', url: 'git@github.com:a1ntwan/groovy_developer.git'
+def testModule(name) {
+    echo "Performing tests"
+    sh(script: "./gradlew -p ${name} test", returnStdout: true)
+}
 
-    if (name == 'homework-4') {
-        dir('bom-examples') {
-            git branch: 'master', credentialsId: 'github', url: 'git@github.com:CycloneDX/bom-examples.git'
+def jibBuild(name) {
+    echo "Performing dockerization and pushing to Dockerhub"
+
+    withCredentials([usernamePassword(credentialsId: 'dockerhub', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+        sh(script: "./gradlew -p ${name} jib -Djib.to.auth.username=${USERNAME} -Djib.to.auth.password=${PASSWORD}", returnStdout: true)
+    }
+}
+
+properties([
+        parameters([
+                extendedChoice(name: 'MODULES', description: 'Choose your branch', multiSelectDelimiter: ',', type: 'PT_CHECKBOX', value: 'groovyProjectWork', visibleItemCount: 6)
+        ])
+])
+
+node {
+    stage("CLEAN WORKSPACE") {
+        cleanWs()
+    }
+
+    stage("CREATE DIRS FOR MODULES") {
+        params['MODULES'].split(',').each { m ->
+            String path = moduleToBranch.findResult { it[m] }
+            new File("${WORKSPACE}/${path}").mkdirs()
         }
+        sh "ls ${WORKSPACE}"
+    }
+
+    stage("PARELLEL GIT CHECKOUT") {
+        def branchedStages = [:]
+
+        params['MODULES'].split(',').each { m ->
+            branchedStages[m] = {
+                stage("PARALLEL GIT CHECKOUT: ${m}") {
+                    String path = moduleToBranch.findResult { it[m] }
+                    dir("${WORKSPACE}/${path}") {
+                        checkout(path)
+                    }
+                }
+            }
+        }
+        parallel branchedStages
+    }
+
+    stage("PARELLEL TEST") {
+        def branchedStages = [:]
+
+        params['MODULES'].split(',').each { m ->
+            branchedStages[m] = {
+                stage("PARALLEL TEST: ${m}") {
+                    String path = moduleToBranch.findResult { it[m] }
+                    dir("${WORKSPACE}/${path}") {
+                        testModule(m)
+                    }
+                }
+            }
+        }
+        parallel branchedStages
+    }
+
+    stage("PARELLEL BUILD AND PUSH") {
+        def branchedStages = [:]
+
+        params['MODULES'].split(',').each { m ->
+            branchedStages[m] = {
+                stage("PARELLEL BUILD AND PUSH: ${m}") {
+                    String path = moduleToBranch.findResult { it[m] }
+                    dir("${WORKSPACE}/${path}") {
+                        PrintSysTime()
+                        jibBuild(m)
+                        CalculateFilesCount()
+                    }
+                }
+            }
+        }
+        parallel branchedStages
     }
 }
---
+++ Jenkinsfile
@@ -0,0 +1,19 @@
+properties([
+  parameters([
+    string(name: 'FIREFOX_TAG', defaultValue: 'test')
+  ])
+])
+
+node {
+    stage("BUILD IMAGE") {
+        echo "Build image for integration tests"
+        git branch: 'main', credentialsId: 'github', url: 'git@github.com:YaroslavErlikh/groovyProjectWork.git'
+
+        docker.withRegistry('https://index.docker.io/v1/', 'dockerhub') {
+
+            dockerImage = docker.build("YaroslavErlikh/firefox:${FIREFOX_TAG}")
+
+            dockerImage.push()
+        }
+    }
+}
